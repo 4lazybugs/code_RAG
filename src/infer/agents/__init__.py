c3090829b3bd7089a6a40b52a3dc792b30e369c5
jwkim <robotjinu25@skku.edu>
2026-08-20 17:30:34 +0900

    trash code moved

--- src/infer/agents/__init__.py
+++ src/infer/agents/__init__.py
@@ -1,12 +1,5 @@
 from .naive_llm import LLM_agent
 from .naive_rag import RAG_agent
-from .iter_rag import IterRAG_agent
-from .gateway import Gateway_agent
-from .gateway_simple import SimGate_agent
-from .judge_hf import Judge_HF
-from .judge_openai import Judge_OpenAI
-from .gateway_rerank import RankGate_agent, Reranker
-from .gateway_token import tokGate_agent
 
 
 __all__ = [
--- src/prompts/__init__.py
+++ src/prompts/__init__.py
@@ -1,5 +1,4 @@
 from .rag_prompt import saq_rag_prompt, saq_rag_prompt_eng, iter_rag_prompt, rag_consult_prompt, rag_paid_consult_prompt, interpretation_prompt
-from .router_prompt import know_prompt, know_prompt_eng
 from .llm_prompt import saq_llm_prompt, saq_llm_prompt_eng
 from .synthesis_prompt import noteLM_prompt, qa_gen_prompt, qa2d_prompt
 
